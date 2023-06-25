406e453b6a2cb2a3e17922929901ff49a358d6c2
Dendi Gustara <dendisamp@gmail.com>
2023-06-26 04:15:46 +0700

    tinggal responcode 400 di deleted note

--- src/service/server.js
+++ src/service/server.js
@@ -1,10 +1,21 @@
 require('dotenv').config();
 const Hapi = require('@hapi/hapi');
+const notes = require('../api/notes');
+const NotesService = require('./inMemory/NotesService');
 
 const init = async () => {
+  const notesService = new NotesService();
+
   const server = Hapi.server({
     host: process.env.HOST,
     port: process.env.PORT,
+  });
+
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+    },
   });
 
   await server.start();
